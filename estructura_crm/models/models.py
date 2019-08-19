3973a9ca671f51ab8505d7abf2650dc4b2c021dc
gilxmarts <gilberto.santiago@xmarts.com>
2019-08-19 12:37:02 -0500

    Ocultar 2 campos

--- estructura_crm/models/models.py
+++ estructura_crm/models/models.py
@@ -7,14 +7,4 @@ class ResPartner(models.Model):
 
 	company_type = fields.Selection([('person','Persona Fisica'),('company','Persona Moral')])
 
-# class estructura_crm(models.Model):
-#     _name = 'estructura_crm.estructura_crm'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
