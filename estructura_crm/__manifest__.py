211f462da447e7fae857283263267ea847f7054e
gilxmarts <gilberto.santiago@xmarts.com>
2019-08-20 11:53:56 -0500

    Group ventas/Responsable

--- estructura_crm/__manifest__.py
+++ estructura_crm/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','crm','contacts'],
+    'depends': ['base','crm','contacts','mail'],
 
     # always loaded
     'data': [
