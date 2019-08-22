3e4c096efc8f97e6d47be49e8e44b83ed1384470
gilxmarts <gilberto.santiago@xmarts.com>
2019-08-22 14:14:44 -0500

    Correo obligatorio

--- estructura_crm/models/models.py
+++ estructura_crm/models/models.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class ResPartner(models.Model):
 	_inherit = "res.partner"
 
 	company_type = fields.Selection([('person','Persona Fisica'),('company','Persona Moral')])
-
-
