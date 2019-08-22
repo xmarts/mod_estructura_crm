70b61849e1a2605c9618f0e2c23edd388754bb19
gilxmarts <gilberto.santiago@xmarts.com>
2019-08-22 17:14:24 -0500

    Empresa cleinte

--- estructura_crm/models/models.py
+++ estructura_crm/models/models.py
@@ -7,3 +7,8 @@ class ResPartner(models.Model):
 	_inherit = "res.partner"
 
 	company_type = fields.Selection([('person','Persona Fisica'),('company','Persona Moral')])
+
+class CrmLead(models.Model):
+	_inherit = "crm.lead"
+
+	partner_name = fields.Many2one('res.partner', "Customer Name", track_visibility='onchange', track_sequence=2, index=True, help='The name of the future partner company that will be created while converting the lead into opportunity')	
