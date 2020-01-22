5cf28e886c564f7d16cb96b22b6c8b0469d7c6d7
David <davidandresdiazgarcia@outlook.com>
2020-01-22 16:25:40 -0600

    update crm

--- estructura_crm/models/models.py
+++ estructura_crm/models/models.py
@@ -13,3 +13,4 @@ class AddDateCrm(models.Model):
 
 	date_now = fields.Datetime(string="Fecha de lead")
 	partner_address_mobile = fields.Char('Partner Contact Mobile', related='partner_id.mobile', readonly=True)
+	phone = fields.Char('Phone', track_visibility='onchange',size=10, track_sequence=5)
